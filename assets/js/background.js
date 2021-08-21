d7191273dae57b6f980cfd99f740a0883e3d98f6
Matias Agelvis <Magelvisdoz@gmail.com>
2021-08-21 12:08:05 -0400

    improved background cross compativility

--- assets/js/background.js
+++ assets/js/background.js
@@ -5,22 +5,30 @@ function getRandomInt(min, max) {
 }
 
 
-document.getElementsByTagName("body")[0].style.setProperty('--fleck-seed', getRandomInt(0, 123456));
-document.getElementsByTagName("body")[0].style.setProperty('--fleck-count', getRandomInt(600, 800));
-
+document.getElementsByClassName("fleck")[0].style.setProperty('--fleck-seed', getRandomInt(0, 123456));
+document.getElementsByClassName("fleck")[0].style.setProperty('--fleck-count', getRandomInt(600, 800));
 
 (async function () {
   if (CSS["paintWorklet"] === undefined) {
     await import("https://unpkg.com/css-paint-polyfill");
   }
 
-  // The code for this worklet can be found here: https://github.com/georgedoescode/houdini-fleck-patterns/blob/main/fleck-worklet.js
   CSS.paintWorklet.addModule("https://unpkg.com/@georgedoescode/houdini-fleck");
-
-  // Fix a weird Safari/Firefox polyfill issue...
-  setTimeout(() => {
-    document.querySelectorAll(".fleck-demo").forEach((el) => {
-      el.style.width = "100%";
-    });
-  }, 250);
+  // CSS.paintWorklet.addModule("assets/js/fleck-worklet.bundle.js");
 })();
+
+// (async function () {
+//   if (CSS["paintWorklet"] === undefined) {
+//     await import("https://unpkg.com/css-paint-polyfill");
+//   }
+
+//   // The code for this worklet can be found here: https://github.com/georgedoescode/houdini-fleck-patterns/blob/main/fleck-worklet.js
+//   CSS.paintWorklet.addModule("https://unpkg.com/@georgedoescode/houdini-fleck");
+
+//   // Fix a weird Safari/Firefox polyfill issue...
+//   setTimeout(() => {
+//     document.querySelectorAll("body").forEach((el) => {
+//       el.style.width = "100%";
+//     });
+//   }, 250);
+// })();
