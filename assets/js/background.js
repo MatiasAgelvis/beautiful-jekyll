b300e41d9a7771b5dd1f7a00be09ea82cf585eae
Matias Agelvis <Magelvisdoz@gmail.com>
2021-08-21 12:09:33 -0400

    cleaned background.js

--- assets/js/background.js
+++ assets/js/background.js
@@ -14,21 +14,4 @@ document.getElementsByClassName("fleck")[0].style.setProperty('--fleck-count', g
   }
 
   CSS.paintWorklet.addModule("https://unpkg.com/@georgedoescode/houdini-fleck");
-  // CSS.paintWorklet.addModule("assets/js/fleck-worklet.bundle.js");
 })();
-
-// (async function () {
-//   if (CSS["paintWorklet"] === undefined) {
-//     await import("https://unpkg.com/css-paint-polyfill");
-//   }
-
-//   // The code for this worklet can be found here: https://github.com/georgedoescode/houdini-fleck-patterns/blob/main/fleck-worklet.js
-//   CSS.paintWorklet.addModule("https://unpkg.com/@georgedoescode/houdini-fleck");
-
-//   // Fix a weird Safari/Firefox polyfill issue...
-//   setTimeout(() => {
-//     document.querySelectorAll("body").forEach((el) => {
-//       el.style.width = "100%";
-//     });
-//   }, 250);
-// })();
